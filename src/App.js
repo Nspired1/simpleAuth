e01799041e63b76598b3a5b57e62ef6edf8f4bd9
Don Spire <don.spire1@gmail.com>
2021-08-20 14:13:07 -0700

    added all user logging comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import GetUser from "./components/GetUser";
+import Logout from "./components/Logout";
 
 function App() {
   return (
     <div className="App">
       <p>This is the react app</p>
+      <Register />
+      <Login />
+      <GetUser />
+      <Logout />
     </div>
   );
 }
