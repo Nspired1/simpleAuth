35aad0ce2bc0ae7c09d98ca5f6184df82dceaf4c
Don Spire <don.spire1@gmail.com>
2021-08-20 14:07:48 -0700

    add Logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,28 @@
+//import { useState } from "react";
+import axios from "axios";
+
+function Logout() {
+  const logout = () => {
+    axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:8081/api/auth/logout",
+    }).then((res) => console.log(res));
+  };
+
+  return (
+    <div>
+      <h1>Logout</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="logout"></label>
+        <button onClick={logout}>Logout</button>
+      </form>
+    </div>
+  );
+}
+
+export default Logout;
