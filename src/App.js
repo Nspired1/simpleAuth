b04292fc9d154eff8acc1245244f5be09db581af
Don Spire <don.spire1@gmail.com>
2021-08-22 23:02:18 -0700

    add React Context API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import "./App.css";
-import Login from "./components/Login";
+import { useContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Context from "./Context";
+import { myContext } from "./Context";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import Register from "./components/Register";
-import GetUser from "./components/GetUser";
-import Logout from "./components/Logout";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import About from "./components/About";
 
 function App() {
+  const ctx = useContext(myContext);
+  ctx;
   return (
     <div className="App">
-      <p>This is the react app</p>
-      <Register />
-      <Login />
-      <GetUser />
-      <Logout />
+      <Context>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Admin path="/admin" component={Admin} />
+            <Home exact path="/" component={Home} />
+          </Switch>
+          <Route exact path="/signin" component={Login} />
+          <Route exact path="/about" component={About} />
+
+          <p>This is the react App</p>
+        </Router>
+      </Context>
     </div>
   );
 }
